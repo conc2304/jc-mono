d3544af1ede184d7a18e4e75a776669102cfe5ca
Jose Conchello <conc2304@users.noreply.github.com>
2025-05-16 12:24:48 -0700

    update dev settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,16 +17,79 @@ export default [
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
+      'react/jsx-no-useless-fragment': ['warn', { allowExpressions: true }],
+      'react-hooks/exhaustive-deps': 'off',
       '@nx/enforce-module-boundaries': [
         'error',
         {
           enforceBuildableLibDependency: true,
-          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+          allow: [],
           depConstraints: [
             {
               sourceTag: '*',
               onlyDependOnLibsWithTags: ['*'],
             },
+          ],
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+          distinctGroup: false,
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@mui/material',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@mui/**',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@lths/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '**/*.+(css|sass|less|scss|pcss|styl)',
+              patternOptions: {
+                dot: true,
+                nocomment: true,
+              },
+              group: 'unknown',
+              position: 'after',
+            },
+            {
+              pattern: '{.,..}/**/*.+(css|sass|less|scss|pcss|styl)',
+              patternOptions: {
+                dot: true,
+                nocomment: true,
+              },
+              group: 'unknown',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          warnOnUnassignedImports: true,
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          groups: [
+            ['builtin', 'external'],
+            ['internal'],
+            // space between
+            ['parent', 'sibling', 'index'],
+            // space between
+            ['type', 'object', 'unknown'],
           ],
         },
       ],
