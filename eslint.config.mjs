19329ec03d377ebd001061a90f19fc5630204d38
Jose Conchello <conc2304@users.noreply.github.com>
2025-07-16 12:52:05 -0700

    finish migration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,55 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
 import nx from '@nx/eslint-plugin';
+import _import from 'eslint-plugin-import';
+import { fixupPluginRules } from '@eslint/compat';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
-export default [
-  ...nx.configs['flat/base'],
-  ...nx.configs['flat/typescript'],
-  ...nx.configs['flat/javascript'],
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  globalIgnores(['**/*']),
   {
-    ignores: [
-      '**/dist',
-      '**/build',
-      '**/.react-router',
-      '**/vite.config.*.timestamp*',
-      '**/vitest.config.*.timestamp*',
-      '**/test-output',
-    ],
+    plugins: {
+      '@nx': nx,
+      import: fixupPluginRules(_import),
+    },
+
+    languageOptions: {
+      globals: {
+        JSX: true,
+        NodeJS: true,
+      },
+    },
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+
     rules: {
-      'react/jsx-no-useless-fragment': ['warn', { allowExpressions: true }],
+      'react/jsx-no-useless-fragment': [
+        'warn',
+        {
+          allowExpressions: true,
+        },
+      ],
+
       'react-hooks/exhaustive-deps': 'off',
+
       '@nx/enforce-module-boundaries': [
         'error',
         {
-          enforceBuildableLibDependency: false,
+          enforceBuildableLibDependency: true,
           allow: [],
+
           depConstraints: [
             {
               sourceTag: '*',
@@ -32,11 +58,13 @@ export default [
           ],
         },
       ],
+
       'import/order': [
         'error',
         {
           'newlines-between': 'always',
           distinctGroup: false,
+
           pathGroups: [
             {
               pattern: 'react',
@@ -60,35 +88,40 @@ export default [
             },
             {
               pattern: '**/*.+(css|sass|less|scss|pcss|styl)',
+
               patternOptions: {
                 dot: true,
                 nocomment: true,
               },
+
               group: 'unknown',
               position: 'after',
             },
             {
               pattern: '{.,..}/**/*.+(css|sass|less|scss|pcss|styl)',
+
               patternOptions: {
                 dot: true,
                 nocomment: true,
               },
+
               group: 'unknown',
               position: 'after',
             },
           ],
+
           pathGroupsExcludedImportTypes: ['react'],
           warnOnUnassignedImports: true,
+
           alphabetize: {
             order: 'asc',
             caseInsensitive: true,
           },
+
           groups: [
             ['builtin', 'external'],
             ['internal'],
-            // space between
             ['parent', 'sibling', 'index'],
-            // space between
             ['type', 'object', 'unknown'],
           ],
         },
@@ -96,17 +129,84 @@ export default [
     },
   },
   {
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.cts',
-      '**/*.mts',
-      '**/*.js',
-      '**/*.jsx',
-      '**/*.cjs',
-      '**/*.mjs',
-    ],
-    // Override or add rules here
+    files: ['**/*.ts', '**/*.tsx'],
+    extends: compat.extends('plugin:@nx/typescript'),
+
+    rules: {
+      'no-unused-vars': 'off',
+
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          ignoreRestSiblings: true,
+          destructuredArrayIgnorePattern: '[A-Z]',
+          caughtErrors: 'none',
+        },
+      ],
+
+      '@typescript-eslint/ban-types': [
+        'error',
+        {
+          types: {
+            '{}': {
+              message:
+                "If you want a type meaning 'any object', you probably want `Record<string, unknown>` instead. If you want a type meaning 'any value', you probably want `unknown` instead. If you want a type meaning 'empty object', you probably want `Record<string, never>` instead.",
+              fixWith: 'Record<string, unknown>',
+            },
+
+            String: {
+              message: 'Use string instead',
+              fixWith: 'string',
+            },
+
+            Number: {
+              message: 'Use number instead',
+              fixWith: 'number',
+            },
+
+            Boolean: {
+              message: 'Use boolean instead',
+              fixWith: 'boolean',
+            },
+
+            Function: {
+              message:
+                'The Function type accepts any function-like value. It provides no type safety when calling the function, which can be a common source of bugs. It also accepts things like class declarations, which will throw at runtime as they will not be called with new. For a function that takes no args and returns nothing use: `()=> void`',
+              fixWith: '(...args: unknown) => unknown',
+            },
+
+            any: {
+              message:
+                '`unknown` is recommended over `any` because it provides safer typing — you have to use type assertion or narrow to a specific type if you want to perform operations on unknown.',
+              fixWith: 'unknown',
+            },
+          },
+
+          extendDefaults: true,
+        },
+      ],
+
+      'no-undef': 'error',
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    extends: compat.extends('plugin:@nx/javascript'),
+
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+    },
+  },
+  {
+    files: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
+
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+
     rules: {},
   },
-];
+]);
