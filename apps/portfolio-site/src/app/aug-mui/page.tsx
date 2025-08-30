68ac9476982ad98e65e46062ec05235607c2c1cc
Jose Conchello <conc2304@users.noreply.github.com>
2025-08-30 16:44:57 -0700

    remove dead projects again...

--- apps/portfolio-site/src/app/aug-mui/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from '@mui/material';
-
-export default function Page() {
-  return (
-    <div>
-      <Typography>Augmented Material UI Home Page</Typography>
-    </div>
-  );
-}
