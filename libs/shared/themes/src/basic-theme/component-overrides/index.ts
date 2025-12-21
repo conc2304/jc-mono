55fb661955ee89a4cb5ee6f21dac93cad8754068
Jose Conchello <conc2304@users.noreply.github.com>
2025-12-20 19:43:02 -0800

    fucking use react router bro you dumb amateur

--- libs/shared/themes/src/basic-theme/component-overrides/index.ts
+++ libs/shared/themes/src/basic-theme/component-overrides/index.ts
@@ -1,12 +1,24 @@
 import { Theme } from '@mui/material';
 import { Components } from '@mui/material/styles';
+import { LinkProps } from '@mui/material/Link';
 
 import BaselineCSS from './baseline';
 import ButtonBase from './button-base';
 import ButtonIcon from './button-icon';
+import { LinkBehavior } from './link-behavior';
 
 export default function getComponentOverrides(theme: Theme): Components {
   return {
+    MuiLink: {
+      defaultProps: {
+        component: LinkBehavior,
+      } as LinkProps,
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        LinkComponent: LinkBehavior,
+      },
+    },
     ...BaselineCSS(theme),
     ...ButtonBase(theme),
     ...ButtonIcon(theme),
